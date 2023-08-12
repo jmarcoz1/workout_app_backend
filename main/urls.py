2bc8f87458d106d5f79c2e26b2490db990ec8555
Jorge <jmararr1@gmail.com>
2023-08-12 12:48:10 +0200

    created initial model structure

--- main/urls.py
+++ main/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-v1/', include('rest_framework.urls')),
-    path('workout/', include('workout_api.urls'))
+    path('api-v1/', include('rest_framework.urls'))
+    # path('workout/', include('workout_api.urls'))
 ]
--- workout_api/models.py
+++ workout_api/models.py
@@ -2,28 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+class Trainee(models.Model):
+    name = models.CharField(max_length=200)
+    born_date = models.DateField(editable=True)
+    def __str__(self):
+        return self.name
+
+class Workout(models.Model):
+    date = models.DateField(auto_now_add=True)
+    traineeId = models.ForeignKey(Trainee, on_delete = models.CASCADE, blank = True, null = True)
+
 class Muscle(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
 
-    pass
-
 class Exercise(models.Model):
     name = models.CharField("name of the exercise", max_length=200)
-    muscles = models.ManyToManyField(Muscle, verbose_name="muscle that is involved in the exercise")
-    pass
+    involved_muscles = models.ManyToManyField(Muscle)
+    def __str__(self):
+        return self.name
 
-class PerformedExercise(models.Model):
-    
-    pass
-
-class Workout(models.Model):
-    pass
-
-class Trainee(models.Model):
-    pass
-
-
-
-
+class Set(models.Model):
+    intensity = models.SmallIntegerField()
+    repetitions = models.SmallIntegerField()
+    exerciseId = models.ManyToManyField(Exercise)
+    workoutId = models.ForeignKey(Workout, on_delete = models.CASCADE, blank = True, null = True)
