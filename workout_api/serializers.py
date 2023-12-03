9c3e7a7533348f8b209e1e4c480234079e24d7fe
Jorge Marco <jmararr1@gmail.com>
2023-12-03 01:08:50 +0100

    working fine

--- workout_api/serializers.py
+++ workout_api/serializers.py
@@ -7,12 +7,6 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "birth_date"]
 
 
-class WorkoutSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Workout
-        fields = ["id", "date", "user", "sets"]
-
-
 class MuscleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Muscle
@@ -26,6 +20,13 @@ class ExerciseSerializer(serializers.ModelSerializer):
 
 
 class SetSerializer(serializers.ModelSerializer):
+    # exercise = serializers.StringRelatedField()
     class Meta:
         model = Set
         fields = ["id", "reps_in_reserve", "repetitions", "is_bodyweight", "weight_lifted", "exercise", "reps_in_reserve"]
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    sets = SetSerializer(many=True)
+    class Meta:
+        model = Workout
+        fields = ["id", "date", "user", "sets"]
